f3e81085825d61fbcce3a756c96069e31ef71316
Sergio Guillen <sa.guillen@uniandes.edu.co>
2023-08-26 19:02:27 -0500

    Configuración del despliegue en Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000'
+    //apiUrl: 'http://localhost:5000'
+    apiUrl: 'https://recetario-application-back-23-7e2f1972de3b.herokuapp.com'
   };
