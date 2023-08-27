52a56c42ed3235a9c3de79f3e14d909a701b989f
Sergio Guillen <sa.guillen@uniandes.edu.co>
2023-08-26 19:31:20 -0500

    Cambios menores

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    //apiUrl: 'http://localhost:5000'
     apiUrl: 'https://recetario-application-back-23-7e2f1972de3b.herokuapp.com'
+    //apiUrl: 'http://localhost:5000'
   };
